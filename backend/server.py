f844fef5e44a07e1cad8fae6232cd609988eeadb
Julio Marshall <marshalljulio46@gmail.com>
2026-02-22 19:42:59 +0000

    auto-commit for 143063ac-9837-4a50-9454-fdbf7253ab52

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, APIRouter, Depends, HTTPException, Request, Header
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
-from fastapi.responses import StreamingResponse
+from fastapi.responses import StreamingResponse, FileResponse
 from dotenv import load_dotenv
 from starlette.middleware.cors import CORSMiddleware
 from motor.motor_asyncio import AsyncIOMotorClient
