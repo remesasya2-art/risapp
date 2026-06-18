be5c17b079290f6647e112dc8a02fdf86fc86535
risapp <remesasya2@gmail.com>
2026-06-18 10:47:09 -0400

    Feat: PIN de seguridad - configurar en perfil + backend con bloqueo escalonado (Fase 1)

--- backend/routes/__init__.py
+++ backend/routes/__init__.py
@@ -31,6 +31,7 @@ from routes.btc_lightning import router as btc_lightning_router
 from routes.btc_admin import router as btc_admin_router
 from routes.adminbrl_bridge import router as adminbrl_bridge_router
 from routes.centro_gestion import router as centro_gestion_router
+from routes.pin import router as pin_router
 
 # Include sub-routers
 api_router.include_router(basic_router)
@@ -58,5 +59,6 @@ api_router.include_router(btc_lightning_router)
 api_router.include_router(btc_admin_router)
 api_router.include_router(adminbrl_bridge_router)
 api_router.include_router(centro_gestion_router)
+api_router.include_router(pin_router)
 
 __all__ = ["api_router"]
